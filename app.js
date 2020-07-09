a1a77454cb1d28df562699f8f781998d3e6ff0e5
jorgechavezrnd <jorgechavezrnd@gmail.com>
2020-07-09 00:54:21 -0400

    Usuario y token listo

--- app.js
+++ app.js
@@ -1,15 +1,29 @@
 // Requires
 var express = require('express');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
 
 // Inicializar variables
 var app = express();
 
 
+// Body Parser
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
+
+
+// Importar rutas
+var appRoutes = require('./routes/app');
+var usuarioRoutes = require('./routes/usuario');
+var loginRoutes = require('./routes/login');
+
+
 // Conexión a la base de datos
 mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB',
-                            { useNewUrlParser: true, useUnifiedTopology: true },
+                            { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true },
                             (err, res) => {
 
     if (err) throw err;
@@ -20,14 +34,9 @@ mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB',
 
 
 // Rutas
-app.get('/', (req, res, next) => {
-
-    res.status(200).json({
-        ok: true,
-        mensaje: 'Peticion realizada correctamente'
-    });
-
-});
+app.use('/usuario', usuarioRoutes);
+app.use('/login', loginRoutes);
+app.use('/', appRoutes);
 
 
 // Escuchar peticiones
