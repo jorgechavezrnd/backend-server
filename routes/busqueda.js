5d44d20e8297bd1305989ea5e21278e9bc67be38
jorgechavezrnd <jorgechavezrnd@gmail.com>
2020-07-12 23:51:43 -0400

    Modificaciones para que la búsqueda funcione, terminada. Middlewares para validar el rol ADMIN_ROLE funcionando

--- routes/busqueda.js
+++ routes/busqueda.js
@@ -84,7 +84,7 @@ function buscarHospitales(busqueda, regex) {
     return new Promise((resolve, reject) => {
 
         Hospital.find({ nombre: regex })
-                .populate('usuario', 'nombre email')
+                .populate('usuario', 'nombre email img')
                 .exec((err, hospitales) => {
 
                     if (err) {
@@ -104,7 +104,7 @@ function buscarMedicos(busqueda, regex) {
     return new Promise((resolve, reject) => {
 
         Medico.find({ nombre: regex })
-              .populate('usuario', 'nombre email')
+              .populate('usuario', 'nombre email img')
               .populate('hospital')
               .exec((err, medicos) => {
 
@@ -125,7 +125,7 @@ function buscarUsuarios(busqueda, regex) {
 
     return new Promise((resolve, reject) => {
 
-        Usuario.find({}, 'nombre email role')
+        Usuario.find({}, 'nombre email role img')
             .or([ { 'nombre': regex }, { 'email': regex } ])
             .exec((err, usuarios) => {
 
--- routes/login.js
+++ routes/login.js
@@ -69,13 +69,16 @@ app.post('/google', async (req, res) => {
                     mensaje: 'Debe de usar su autenticación normal'
                 });
             } else {
+                usuarioDB.password = ':)';
+
                 var token = jwt.sign({ usuario: usuarioDB }, SEED, { expiresIn: 14400 }); // 4 horas
 
                 res.status(200).json({
                     ok: true,
                     usuario: usuarioDB,
                     token: token,
-                    id: usuarioDB._id
+                    id: usuarioDB._id,
+                    menu: obtenerMenu(usuarioDB.role)
                 });
             }
 
@@ -91,13 +94,22 @@ app.post('/google', async (req, res) => {
 
             usuario.save((err, usuarioDB) => {
 
+                if (err) {
+                    return res.status(500).json({
+                        ok: false,
+                        mensaje: 'Error al crear usuario - google',
+                        errors: err
+                    });
+                }
+
                 var token = jwt.sign({ usuario: usuarioDB }, SEED, { expiresIn: 14400 }); // 4 horas
 
                 res.status(200).json({
                     ok: true,
                     usuario: usuarioDB,
                     token: token,
-                    id: usuarioDB._id
+                    id: usuarioDB._id,
+                    menu: obtenerMenu(usuarioDB.role)
                 });
 
             });
@@ -150,12 +162,47 @@ app.post('/', (req, res) => {
             ok: true,
             usuario: usuarioDB,
             token: token,
-            id: usuarioDB._id
+            id: usuarioDB._id,
+            menu: obtenerMenu(usuarioDB.role)
         });
 
     });
 
 });
+
+
+function obtenerMenu(ROLE) {
+
+    var menu = [
+        {
+            titulo: 'Principal',
+            icono: 'mdi mdi-gauge',
+            submenu: [
+                { titulo: 'Dashboard', url: '/dashboard' },
+                { titulo: 'ProgressBar', url: '/progress' },
+                { titulo: 'Gráficas', url: '/graficas1' },
+                { titulo: 'Promesas', url: '/promesas' },
+                { titulo: 'RxJs', url: '/rxjs' }
+            ]
+        },
+        {
+            titulo: 'Mantenimiento',
+            icono: 'mdi mdi-folder-lock-open',
+            submenu: [
+                // { titulo: 'Usuarios', url: '/usuarios' },
+                { titulo: 'Hospitales', url: '/hospitales' },
+                { titulo: 'Médicos', url: '/medicos' },
+            ]
+        }
+    ];
+
+    if (ROLE === 'ADMIN_ROLE') {
+        menu[1].submenu.unshift( { titulo: 'Usuarios', url: '/usuarios' } );
+    }
+
+    return menu;
+
+}
 
 
 module.exports = app;
